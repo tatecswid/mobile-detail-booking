dcd78ac4f014f5ed89a669e7278af67f33fd39e2
tatecswid <tatecswid@gmail.com>
2026-04-21 22:52:49 -0400

    rework database connection

--- backend/config/supabaseClient.js
+++ backend/config/supabaseClient.js
@@ -1,6 +1,9 @@
-const postgres = require('postgres');
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
 
-const connectionString = process.env.DATABASE_URL;
-const sql = postgres(connectionString);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
-module.exports = sql;
+module.exports = supabase;
--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const sql = require("../config/supabaseClient")
+const supabase = require("../config/supabaseClient")
 
 router.get('/', async (req, res) => {
-    const users = await sql`SELECT * from customer_contact`;
-    res.status(200).json(users)
+    const { data, error } = await supabase.from('customer_contact').select('*');
+    res.status(200).json(data)
 })
 
 module.exports = router;
